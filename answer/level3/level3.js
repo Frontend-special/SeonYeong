0d4d549c9b7dc38292df4579b89a17884b89e886
seonyeong719 <sy904280@gmail.com>
2023-01-03 23:58:43 +0900

    Design: level3 분석 추가 수정
    분석 추가 수정 하였습니다

--- answer/level3/level3.js
+++ answer/level3/level3.js
@@ -10,6 +10,8 @@ const $button = document.querySelector("#submit_button");
 const $list = document.querySelector("#ingredient-list");
 const INGREDIENT_LIST = new Map();
 
+//태그 요소들을 다 불러 와준다.
+
 function deleteIngredient(e) {
     if (!e.target.matches("button")) return;
     const $tr = e.target.closest("tr");
@@ -17,6 +19,17 @@ function deleteIngredient(e) {
     $tr.remove();
     INGREDIENT_LIST.delete(ingredient);
 }
+
+/*
+이건 버튼 클릭시 데이터를 삭제해주는 함수인데, 
+예외처리로 ...
+
+$tr에 타겟의 가장 가까운 tr을 찾아준다. --> 밑에 나오는 tr이 우리가 추가할 재료와 용량인데 
+그것들을 삭제 해주기 위해 deleteIngredient함수 안에 tr을 불러와 할당해준듯 하다.
+상수 ingredient에 tr의 td 텍스트들(재료와 용량)을 할당해주고, 버튼을 클릭시 그것들을 지워준다.
+그리고 어떤걸 지워주냐면 INGREDIENT_LIST 안에 있는 ingredient 텍스트들을(재료와 용량) 지워준다.
+
+*/
 
 $form.addEventListener("submit", (e) => {
     e.preventDefault();
@@ -41,8 +54,22 @@ $form.addEventListener("submit", (e) => {
     $weight.value = "";
 });
 
+/*
+1. 상수 ingredientValue,weightValue에 값을 할당해준다 
+2. 예외처리로 INGREDIENT_LIST에 ingredientValue 가 들어있으면 alert로 이미 존재한다고 알림.
+3. 예외처리로 ingredientValue가 없거나 weightValue가 없으면 alret로 입력해주세요 로 알림.
+4. tr 요소를 추가 생성해준다.
+5. tr.innerHTML td 태그들을 넣어준다 
+6. tr.addEventListener("click", deleteIngredient);을 써준 이유는..
+   삭제하는 클릭 이벤트를 주기 위함인 것 같다.
+7. INGREDIENT_LIST에 재료와 용량의 밸류값들을 넣어주고 table태그 요소에 tr의 요소들을 넣어준다.
+8. 그 밑에 $ingredient.value = "" <-- 이것처럼 따옴표를 해준 이유는 잘 모르겠다..
+
+*/
+
 $button.addEventListener("click", () => {
     if ($list.children.length > 0) $list.innerHTML = "";
+    console.log($list.children.length);
     INGREDIENT_LIST.forEach((weight, ingredient) => {
         const li = document.createElement("li");
         li.innerText = `${ingredient} : ${weight}`;
@@ -50,7 +77,5 @@ $button.addEventListener("click", () => {
     });
 });
 /*
-테이블 안에 td 태그 추가하는법
- append, createEle 차이점 - 궁금
-
+1. 
 */
