dbc5390711d1b9bb365f52f6cec192aeb999751f
seonyeong719 <sy904280@gmail.com>
2023-01-04 17:25:03 +0900

    Add: level4 분석 추가
    분석 미완성

--- answer/level4/level4.js
+++ answer/level4/level4.js
@@ -2,66 +2,74 @@
 페이지네이션 구현하기
 */
 
-const $pageList = document.querySelector('.page_list');
-const $nextPage = document.querySelector('.btn_nav.next');
-const $prevPage = document.querySelector('.btn_nav.pre');
+const $pageList = document.querySelector(".page_list");
+const $nextPage = document.querySelector(".btn_nav.next");
+const $prevPage = document.querySelector(".btn_nav.pre");
 
 let totalPage = 80;
 let PAGE_LST = [];
 let currentPageIndx = 0;
-let currentPage = new URLSearchParams(location.search).get('page') || 1;
+let currentPage = new URLSearchParams(location.search).get("page") || 1;
 
 Array.prototype.division = function (div) {
-  const arr = this;
-  const result = [];
-  const len = arr.length;
-  for (let i = 0; i < len; i += div) {
-    result.push(arr.slice(i, i + div));
-  }
-  return result;
+    const arr = this;
+    const result = [];
+    const len = arr.length;
+    for (let i = 0; i < len; i += div) {
+        result.push(arr.slice(i, i + div));
+    }
+    return result;
 };
+/*
 
+*/
 (function pagaNation(totalPage, currentPage) {
-  const paageNation_list = [];
-  const paageNation_limit = 10;
-  currentPageIndx = Math.ceil(currentPage / paageNation_limit) - 1;
-  for (let i = 1; i <= totalPage; i++) {
-    paageNation_list.push(i);
-  }
-  PAGE_LST = paageNation_list.division(paageNation_limit);
-  renderPageNation();
+    const paageNation_list = [];
+    const paageNation_limit = 10;
+    currentPageIndx = Math.ceil(currentPage / paageNation_limit) - 1;
+    for (let i = 1; i <= totalPage; i++) {
+        paageNation_list.push(i);
+    }
+    PAGE_LST = paageNation_list.division(paageNation_limit);
+    renderPageNation();
 })(totalPage, currentPage);
+
+/*
+1. 즉시 실행 함수로 선언해줬다 
+2. 상수에 빈 배열과 , 페이지 리밋은 10으로 할당해줌
+3. currentPageIndx은 
+*/
 
 // 페이지네이션 랜더링
 function renderPageNation() {
-  const pageNationList = PAGE_LST[currentPageIndx];
-  $pageList.innerHTML = pageNationList
-    .map((page) => {
-      return `<a href="?page=${page}" class="nav_page" data-page="${page}">${page}</a>`;
-    })
-    .join('');
+    const pageNationList = PAGE_LST[currentPageIndx];
+    $pageList.innerHTML = pageNationList
+        .map((page) => {
+            return `<a href="?page=${page}" class="nav_page" data-page="${page}">${page}</a>`;
+        })
+        .join("");
 
-  const pageList = document.querySelector('.page_list').children;
-  for (let i = 0; i < pageList.length; i++) {
-    if (pageList[i].dataset.page == currentPage) {
-      pageList[i].classList.add('on');
+    const pageList = document.querySelector(".page_list").children;
+    for (let i = 0; i < pageList.length; i++) {
+        if (pageList[i].dataset.page == currentPage) {
+            pageList[i].classList.add("on");
+        }
     }
-  }
 }
 
 // 넥스트 페이지 그룹
 function nextPageGroup() {
-  if (!(currentPageIndx < PAGE_LST.length - 1)) return;
-  currentPageIndx++;
-  renderPageNation();
+    if (!(currentPageIndx < PAGE_LST.length - 1)) return;
+    currentPageIndx++;
+    renderPageNation();
 }
 
 // 이전 페이지 그룹
 function prevPageGroup() {
-  if (!(currentPageIndx > 0)) return;
-  currentPageIndx--;
-  renderPageNation();
+    if (!(currentPageIndx > 0)) return;
+    currentPageIndx--;
+    renderPageNation();
 }
 
-$nextPage.addEventListener('click', nextPageGroup);
-$prevPage.addEventListener('click', prevPageGroup);
+$nextPage.addEventListener("click", nextPageGroup);
+$prevPage.addEventListener("click", prevPageGroup);
